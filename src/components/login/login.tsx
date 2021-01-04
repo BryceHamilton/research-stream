fec8d51ae610dd85b08ec3e243eae170a1dffb0a
BryceHamilton <bhamilton@haivision.com>
2021-01-04 23:41:05 +0000

    custom login component

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -3,19 +3,33 @@ import { GoogleLogin } from 'react-google-login';
 import { useDispatch } from 'react-redux';
 import { userActions } from '../../store/user/actions';
 import GoogleButton from '../google-button';
+import PrimaryButton from '../primary-button';
 require('dotenv').config();
 
 const { REACT_APP_GOOGLE_CLIENT_ID } = process.env;
 
-const Login: React.FunctionComponent = () => {
+const Login: React.FunctionComponent<{ custom?: string }> = ({ custom }) => {
   const dispatch = useDispatch();
   const handleLogin = (response: GoogleResponse): void => {
-    userActions.login(response)(dispatch);
+    userActions.login(dispatch)(response);
   };
+
+  const LoginButton: React.FunctionComponent<{
+    onClick: any;
+    disabled?: boolean;
+  }> = ({ onClick, disabled }) =>
+    custom ? (
+      <PrimaryButton to='' onClick={onClick}>
+        {custom}
+      </PrimaryButton>
+    ) : (
+      <GoogleButton onClick={onClick} disabled={disabled} />
+    );
+
   return (
     <GoogleLogin
       clientId={REACT_APP_GOOGLE_CLIENT_ID || ''}
-      render={(renderProps) => <GoogleButton {...renderProps} />}
+      render={(renderProps) => <LoginButton {...renderProps} />}
       onSuccess={handleLogin}
       onFailure={handleLogin}
       cookiePolicy={'single_host_origin'}
